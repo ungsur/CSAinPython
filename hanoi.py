942d05cb6ea288eda3451b86921e5fcee6bfc7cc
ungsur <ungsunan@gmail.com>
2021-10-25 19:07:16 -0400

    adding hanoi model

---
+++ hanoi.py
@@ -0,0 +1,22 @@
+class Stack:
+    def __init__(self):
+        self._container = []
+
+    def push(self, item):
+        self._container.append(item)
+
+    def pop(self):
+        return self._container.pop()
+
+    def __repr__(self):
+        return repr(self._container)
+
+def main():
+    num_discs = 3
+    tower_a = Stack()
+    for i in range(1, num_discs + 1):
+        tower_a.push(i)
+    print(tower_a)
+    
+if __name__ == '__main__':
+    main()
