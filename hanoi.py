23321306069b2199047d43cf4150337ee56e2b91
ungsur <ungsunan@gmail.com>
2021-10-30 16:05:54 -0400

    hanoi commit

--- hanoi.py
+++ hanoi.py
@@ -11,13 +11,30 @@ class Stack:
     def __repr__(self):
         return repr(self._container)
 
+
+def hanoi(begin, end, temp, n):
+    print("execution")
+    if n == 1:
+        end.push(begin.pop())
+    else:     #123 0 0  3
+        hanoi(begin, temp, end, n-1)    #123  0 0 2
+        hanoi(begin, end, temp, 1)      #23 1 0 1
+        hanoi(temp, end, begin, n-1)    #3 1
+
+
 def main():
     num_discs = 3
     tower_a = Stack()
-    
-    for i in range(1, num_discs + 1):
+    tower_b = Stack()
+    tower_c = Stack()
+    for i in range(1,num_discs + 1):
         tower_a.push(i)
     print(tower_a)
+    hanoi(tower_a, tower_c, tower_b, num_discs)
+    print(tower_a)
+    print(tower_b) 
+    print(tower_c)
+
 
 
 if __name__ == '__main__':
